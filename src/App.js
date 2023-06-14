22f78ed055e51c0d25656104684164ca510e9093
Musyoki-Wambua <wjosephmusyoki@gmail.com>
2023-06-14 10:42:35 +0300

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ChatBot from 'react-simple-chatbot'
+import { Segment } from 'semantic-ui-react';
 
 function App() {
+  const steps = [
+   {
+    id:'Greet', 
+    message:'Hello, welcome to our website',
+    trigger:'Ask Name'
+   }, 
+   {
+    id:'Ask Name',
+    message:'Please enter your name',
+    trigger:'validate'
+   },
+   {
+    id: "validate",
+    user:true, // validator: (value) => value.length > 0 ? false : 'Name is required', 
+    trigger: 'Name'
+   }, 
+   {
+    id:"Name",
+    message:'Hi {previousValue}, Please select your issue',
+    trigger: 'issues'
+   },
+   {
+    id: 'issues',
+    options: [
+      {value: 'React', label:'React', trigger:"React"},
+      {value: 'Angular', label:'Angular', trigger:"Angular"}
+    ],
+   }, 
+   {
+    id: 'React',
+    message:'Thanks for telling us about your React issue',
+    end:true,
+   }, 
+   {
+    id: 'Angular',
+    message:'Thanks for telling us about your Angular issue',
+    end:true,
+   }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <Segment floated='right' >
+    <ChatBot steps={steps}/>
+   </Segment>
   );
 }
 
